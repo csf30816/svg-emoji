e97364dac6676152f072145f6cc770488d9b2187
csf30816 <elijahporter@zoho.com>
2017-05-20 09:58:13 -1000

    Update emoji.js

--- src/emoji.js
+++ src/emoji.js
@@ -6,10 +6,12 @@ window.onload = function () {
 	// Smile 🙂
 	replaceTextByImage(':\\)', 'https://csf30816.github.io/svg-emoji/emojis/smile.svg');
 	replaceTextByImage(':smile:', 'https://csf30816.github.io/svg-emoji/emojis/smile.svg');
+	replaceTextByImage('🙂', 'https://csf30816.github.io/svg-emoji/emojis/smile.svg');
 	
 	// Tongue 😛
 	replaceTextByImage(':P', 'https://csf30816.github.io/svg-emoji/emojis/tongue.svg');
 	replaceTextByImage(':tongue:', 'https://csf30816.github.io/svg-emoji/emojis/tongue.svg');
+	replaceTextByImage('😛', 'https://csf30816.github.io/svg-emoji/emojis/tongue.svg');
 	
 	// Big Smile 😀
 	replaceTextByImage(':D', 'https://csf30816.github.io/svg-emoji/emojis/big-smile.svg');
@@ -19,50 +21,64 @@ window.onload = function () {
 	// Frown ☹ 🙁
 	replaceTextByImage(':\\(', 'https://csf30816.github.io/svg-emoji/emojis/frown.svg');
 	replaceTextByImage(':frown:', 'https://csf30816.github.io/svg-emoji/emojis/frown.svg');
+	replaceTextByImage('☹', 'https://csf30816.github.io/svg-emoji/emojis/frown.svg');
+	replaceTextByImage('🙁', 'https://csf30816.github.io/svg-emoji/emojis/frown.svg');
 	
 	// Wink 😉
 	replaceTextByImage(';\\)', 'https://csf30816.github.io/svg-emoji/emojis/wink.svg');
 	replaceTextByImage(':wink:', 'https://csf30816.github.io/svg-emoji/emojis/wink.svg');
+	replaceTextByImage('😉', 'https://csf30816.github.io/svg-emoji/emojis/wink.svg');
 	
 	// Dizzy 😵
 	replaceTextByImage('xO', 'https://csf30816.github.io/svg-emoji/emojis/dead.svg');
 	replaceTextByImage(':dizzy:', 'https://csf30816.github.io/svg-emoji/emojis/dead.svg');
 	replaceTextByImage(':dead:', 'https://csf30816.github.io/svg-emoji/emojis/dead.svg');
+	replaceTextByImage('😵', 'https://csf30816.github.io/svg-emoji/emojis/dead.svg');
 	
 	// Cry 😭 😢
 	replaceTextByImage(':crying:', 'https://csf30816.github.io/svg-emoji/emojis/cry.svg');
 	replaceTextByImage(':cry:', 'https://csf30816.github.io/svg-emoji/emojis/cry.svg');
+	replaceTextByImage('😭', 'https://csf30816.github.io/svg-emoji/emojis/cry.svg');
+	replaceTextByImage('😢', 'https://csf30816.github.io/svg-emoji/emojis/cry.svg');
 	
 	// Big Frown 😦
 	replaceTextByImage('D:', 'https://csf30816.github.io/svg-emoji/emojis/big-frown.svg');
 	replaceTextByImage(':big-frown:', 'https://csf30816.github.io/svg-emoji/emojis/big-frown.svg');
 	replaceTextByImage(':gasp:', 'https://csf30816.github.io/svg-emoji/emojis/big-frown.svg');
+	replaceTextByImage('😦', 'https://csf30816.github.io/svg-emoji/emojis/big-frown.svg');
 	
 	// Heart Eyes 😍
 	replaceTextByImage(':heart-eyes:', 'https://csf30816.github.io/svg-emoji/emojis/heart-eyes.svg');
+	replaceTextByImage('😍', 'https://csf30816.github.io/svg-emoji/emojis/heart-eyes.svg');
 	
 	// Neutral 😐
 	replaceTextByImage(':neutral:', 'https://csf30816.github.io/svg-emoji/emojis/neutral.svg');
 	replaceTextByImage(':\\|', 'https://csf30816.github.io/svg-emoji/emojis/neutral.svg');
 	replaceTextByImage(':plain:', 'https://csf30816.github.io/svg-emoji/emojis/neutral.svg');
+	replaceTextByImage('😐', 'https://csf30816.github.io/svg-emoji/emojis/neutral.svg');
 	
 	// Raging 😡
 	replaceTextByImage(':raging:', 'https://csf30816.github.io/svg-emoji/emojis/raging.svg');
 	replaceTextByImage(':angry-red:', 'https://csf30816.github.io/svg-emoji/emojis/raging.svg');
+	replaceTextByImage('😡', 'https://csf30816.github.io/svg-emoji/emojis/raging.svg');
 	
 	// Mad 😠
 	replaceTextByImage(':angry:', 'https://csf30816.github.io/svg-emoji/emojis/mad.svg');
 	replaceTextByImage(':mad:', 'https://csf30816.github.io/svg-emoji/emojis/mad.svg');
+	replaceTextByImage('😠', 'https://csf30816.github.io/svg-emoji/emojis/mad.svg');
 	
 	// Teeth 😁
 	replaceTextByImage(':teeth:', 'https://csf30816.github.io/svg-emoji/emojis/teeth.svg');
 	replaceTextByImage(':wide-smile:', 'https://csf30816.github.io/svg-emoji/emojis/teeth.svg');
+	replaceTextByImage('😁', 'https://csf30816.github.io/svg-emoji/emojis/teeth.svg');
 	
 	// Thumbs Up 👍
 	replaceTextByImage(':thumbs-up:', 'https://csf30816.github.io/svg-emoji/emojis/thumbs-up.svg');
 	replaceTextByImage(':up:', 'https://csf30816.github.io/svg-emoji/emojis/thumbs-up.svg');
+	replaceTextByImage('👍', 'https://csf30816.github.io/svg-emoji/emojis/thumbs-up.svg');
 	
 	// Thumbs Down 👎
 	replaceTextByImage(':thumbs-down:', 'https://csf30816.github.io/svg-emoji/emojis/thumbs-down.svg');
 	replaceTextByImage(':down:', 'https://csf30816.github.io/svg-emoji/emojis/thumbs-down.svg');
+	replaceTextByImage('👎', 'https://csf30816.github.io/svg-emoji/emojis/thumbs-down.svg');
 };
